9f3285819ca639799abb4f092180b1adeae704d7
DI <DI@DIs-MacBook-Pro.(none)>
2012-10-15 01:24:50 -0400

    udpate

--- app.py
+++ app.py
@@ -20,9 +20,7 @@ connect('mydata', host=os.environ.get('MONGOLAB_URI'))
 app.logger.debug("Connecting to MongoLabs")
 
 # hardcoded categories for the checkboxes on the form
-categories = ['GEAR','TECH','FASHION','BODY','ART','RIDE','PLACE']
-#image= ['x1.jpeg','makerbot.jpeg']
-
+categories = ['web','physical computing','software','video','music','installation','assistive technology','developing nations','business','social networks']
 
 # --------- Routes ----------
 
@@ -30,11 +28,10 @@ categories = ['GEAR','TECH','FASHION','BODY','ART','RIDE','PLACE']
 @app.route("/", methods=['GET','POST'])
 def index():
 
-	app.logger.debug(request.form.getlist('categories'))
-
 	# get Idea form from models.py
 	idea_form = models.IdeaForm(request.form)
 	
+	# if form was submitted and it is valid...
 	if request.method == "POST" and idea_form.validate():
 	
 		# get form data - create new idea
@@ -43,16 +40,20 @@ def index():
 		idea.title = request.form.get('title','no title')
 		idea.slug = slugify(idea.title + " " + idea.creator)
 		idea.idea = request.form.get('idea','')
-		idea.categories = request.form.getlist('categories')
-		idea.categories = request.form.getlist('categories')
+		idea.categories = request.form.getlist('categories') # getlist will pull multiple items 'categories' into a list
 		
-		idea.save()
+		idea.save() # save it
 
+		# redirect to the new idea page
 		return redirect('/ideas/%s' % idea.slug)
 
 	else:
 
-		if request.form.getlist('categories'):
+		# for form management, checkboxes are weird (in wtforms)
+		# prepare checklist items for form
+		# you'll need to take the form checkboxes submitted
+		# and idea_form.categories list needs to be populated.
+		if request.method=="POST" and request.form.getlist('categories'):
 			for c in request.form.getlist('categories'):
 				idea_form.categories.append_entry(c)
 
@@ -65,14 +66,19 @@ def index():
 
 		return render_template("main.html", **templateData)
 
+# Display all ideas for a specific category
 @app.route("/category/<cat_name>")
 def by_category(cat_name):
 
+	# try and get ideas where cat_name is inside the categories list
 	try:
 		ideas = models.Idea.objects(categories=cat_name)
+
+	# not found, abort w/ 404 page
 	except:
 		abort(404)
 
+	# prepare data for template
 	templateData = {
 		'current_category' : {
 			'slug' : cat_name,
@@ -82,8 +88,8 @@ def by_category(cat_name):
 		'categories' : categories
 	}
 
+	# render and return template
 	return render_template('category_listing.html', **templateData)
-
 
 
 @app.route("/ideas/<idea_slug>")
@@ -102,7 +108,55 @@ def idea_display(idea_slug):
 
 	# render and return the template
 	return render_template('idea_entry.html', **templateData)
+
+@app.route("/ideas/<idea_slug>/edit", methods=['GET','POST'])
+def idea_edit(idea_slug):
+
 	
+	# try and get the Idea from the database / 404 if not found
+	try:
+		idea = models.Idea.objects.get(slug=idea_slug)
+		
+		# get Idea form from models.py
+		# if http post, populate with user submitted form data
+		# else, populate the form with the database record
+		idea_form = models.IdeaForm(request.form, obj=idea)	
+	except:
+		abort(404)
+
+	# was post received and was the form valid?
+	if request.method == "POST" and idea_form.validate():
+	
+		# get form data - update a few fields
+		# note we're skipping the update of slug (incase anyone has previously bookmarked)
+		idea.creator = request.form.get('creator','anonymous')
+		idea.title = request.form.get('title','no title')
+		idea.idea = request.form.get('idea','')
+		idea.categories = request.form.getlist('categories')
+
+		idea.save() # save changes
+
+		return redirect('/ideas/%s/edit' % idea.slug)
+
+	else:
+
+		# for form management, checkboxes are weird (in wtforms)
+		# prepare checklist items for form
+		# you'll need to take the form checkboxes submitted
+		# and idea_form.categories list needs to be populated.
+		if request.method=="POST" and request.form.getlist('categories'):
+			for c in request.form.getlist('categories'):
+				idea_form.categories.append_entry(c)
+
+		templateData = {
+			'categories' : categories,
+			'form' : idea_form,
+			'idea' : idea
+		}
+
+		return render_template("idea_edit.html", **templateData)
+
+
 @app.route("/ideas/<idea_id>/comment", methods=['POST'])
 def idea_comment(idea_id):
 
@@ -159,6 +213,7 @@ if __name__ == "__main__":
 	
 	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
 	app.run(host='0.0.0.0', port=port)
+
 
 
 	
